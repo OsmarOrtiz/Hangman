3e71725844bc3224a0ba54b51307b90dd12fccdf
OsmarOrtiz <147085021+OsmarOrtiz@users.noreply.github.com>
2023-10-14 14:29:58 -0700

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -8,17 +8,29 @@ def get_valid_word(words):
         word = random.choice(word)
     return word
 
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)   #asigna la cant de palabras en un set    
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
 
+    #user imput
+    while len(word_letters) > 0:
+        # letras usadas
+        print ('You have used this letters', ' '.join(used_letters))
 
-    user_letter = input('Guess a letter: ').upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
-                    
-    elif user_letter in used_letters:
-        print('You already used that letter, Try again.')
-                    
-    else:
-        print('invalid character try again')             
-
+        #shows only the guessed words
+        word_list = [letter if letter in used_letters else '_' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+            
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+        
+        elif user_letter in used_letters:
+            print('You already used that letter, Try again.')
+        
+        else:
+            print('invalid character try again')             
